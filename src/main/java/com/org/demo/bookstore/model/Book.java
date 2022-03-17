be67404666e1fd330aa0cc9981824d5f5dfc3eb9
NAGARRO\sachinpipal <sachin.pipal@nagarro.com>
2022-03-17 16:35:34 +0530

    entities created

---
+++ src/main/java/com/org/demo/bookstore/model/Book.java
@@ -0,0 +1,29 @@
+package com.org.demo.bookstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * Representation of Book Table
+ */
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String isbn;
+	private String title;
+	private String author;
+	private Double price;
+}
---
+++ src/main/java/com/org/demo/bookstore/response/BookResponseVO.java
@@ -0,0 +1,14 @@
+package com.org.demo.bookstore.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponseVO {
+
+	private String title;
+
+}
