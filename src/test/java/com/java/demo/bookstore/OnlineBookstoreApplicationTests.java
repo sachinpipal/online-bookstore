d6469aa2819f164509412d118c5adcba7c76e2f2
NAGARRO\sachinpipal <sachin.pipal@nagarro.com>
2022-03-17 14:02:09 +0530

    First commit:Creating repo

---
+++ src/test/java/com/java/demo/bookstore/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.demo.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
