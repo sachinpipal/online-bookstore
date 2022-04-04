8cb145999e71b44910b9d7e20a5467ae35cc29ae
NAGARRO\sachinpipal <sachin.pipal@nagarro.com>
2022-04-04 14:23:50 +0530

    readme and synchronisation

--- README.md
+++ README.md
@@ -3,4 +3,44 @@ online bookstore master branch
 
 STESP to RUN
 ===========================
+1.Create  database using Db script.
+create database bookstoreApp;
+use bookstoreApp;
+Execute the postgresql script 
+/online-bookstore/src/main/resources/schema.sql
+
+2.Execute the main method in /online-bookstore/src/main/java/com/org/demo/bookstore/OnlineBookstoreApplication.java
+
+3.Add book to the book store API.
+
+URL:http://localhost:8083/online-bookstore/v1/books/add-book
+
+Request Body:{
+    "isbn":"121323",
+    "title":"abc",
+    "author":"pqr",
+    "quantity":2,
+    "price":"10"
+}
+
+4.Search book API.
+http://localhost:8083/online-bookstore/v1/books/search?author=xyz
+
+5.Search media coverage of book API.
+http://localhost:8083/online-bookstore/v1/books/search-media-coverage/title/provident
+
+6.Buy book API.
+http://localhost:8083/online-bookstore/v1/books/buy-book
+
+    [
+        {
+            "title": "abc",
+            "quantity": 1
+        },
+        {
+            "title": "and",
+            "quantity": 1
+        }
+    ]
+
 
--- src/main/java/com/org/demo/bookstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/org/demo/bookstore/service/impl/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,7 @@ public class OrderServiceImpl implements OrderService {
 	private BookStoreRepository bookStoreRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	private Lock lock = new ReentrantLock();
 
 	@Override
 	public OrderResponseVO buyBook(List<OrderRequestVO> orderRequestVOList) {
@@ -37,33 +40,40 @@ public class OrderServiceImpl implements OrderService {
 		List<String> titleList = new ArrayList<>();
 		List<Book> books = new ArrayList<>();
 		try {
-			titleList = orderRequestVOList.stream().map(OrderRequestVO::getTitle).collect(Collectors.toList());
-			String orderId = "ODR" + new Random().nextInt(999999);
-			for (OrderRequestVO orderRequestVO : orderRequestVOList) {
-				Optional<Book> optionalBook = bookStoreRepository.getBookByTitleAndQuantity(orderRequestVO.getTitle(),
-						orderRequestVO.getQuantity());
-				if (optionalBook.isPresent()) {
-					Order order = new Order();
-					Book book = optionalBook.get();
-					order.setBook(book);
-					order.setQuantity(orderRequestVO.getQuantity());
-					order.setOrderId(orderId);
-					orders.add(order);
-					book.setQuantity(book.getQuantity() - orderRequestVO.getQuantity());
-					books.add(book);
-				} else {
-					throw new BookStoreBaseException(
-							"Order quantity not matched for title :" + orderRequestVO.getTitle());
+			if (lock.tryLock()) {
+				titleList = orderRequestVOList.stream().map(OrderRequestVO::getTitle).collect(Collectors.toList());
+				String orderId = "ODR" + new Random().nextInt(999999);
+				for (OrderRequestVO orderRequestVO : orderRequestVOList) {
+					Optional<Book> optionalBook = bookStoreRepository
+							.getBookByTitleAndQuantity(orderRequestVO.getTitle(), orderRequestVO.getQuantity());
+					if (optionalBook.isPresent()) {
+						Order order = new Order();
+						Book book = optionalBook.get();
+						order.setBook(book);
+						order.setQuantity(orderRequestVO.getQuantity());
+						order.setOrderId(orderId);
+						orders.add(order);
+						book.setQuantity(book.getQuantity() - orderRequestVO.getQuantity());
+						books.add(book);
+					} else {
+						throw new BookStoreBaseException(
+								"Order quantity not matched for title :" + orderRequestVO.getTitle());
+					}
 				}
+				orderRepository.saveAll(orders);
+				bookStoreRepository.saveAll(books);
+				return new OrderResponseVO(titleList, orderId);
+			} else {
+				throw new BookStoreBaseException("Exception occurred while ordering " + titleList);
 			}
-			orderRepository.saveAll(orders);
-			bookStoreRepository.saveAll(books);
-			return new OrderResponseVO(titleList, orderId);
+
 		} catch (Exception ex) {
 			log.error("Exception occured  " + this.getClass().getName() + "method :buyBook () for titleList : "
 					+ titleList, ex.getCause());
 			throw new BookStoreBaseException("No result found with given search, titleList: " + titleList,
 					ex.getCause());
+		} finally {
+			lock.unlock();
 		}
 	}
 }
